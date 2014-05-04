d45ebd2f79492d20b591ca9634bc4e88948bd9f8
icoz <icoz.vt@gmail.com>
2014-05-04 21:58:10 +0400

    init

---
+++ db.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+__author__ = 'icoz'
+
+MONGO_HOST = '127.0.0.1'
+MONGO_PORT = 27017
+MONGO_DATABASE = 'syslog'
+
+db = MongoClient(host=MONGO_HOST, port=MONGO_PORT)[MONGO_DATABASE]
---
+++ pysyslog_analyzer.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/gen_info')
+def gen_info():
+    return 'Done'
+
+
+if __name__ == '__main__':
+    app.run()
