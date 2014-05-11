e5c5b83256e9985010592b696c20723522fc7a6c
icoz <icoz.vt@gmail.com>
2014-05-11 20:13:37 +0400

    Written json_apps(), json_servers().
    Added get_info() (/get_info/) - form for requests to DB
    Written filter on:
    - host
    - app
    - prio
    - regexp on message
    
    Added request_form.html (form for request).

--- pysyslog_analyzer.py
+++ pysyslog_analyzer.py
@@ -3,7 +3,7 @@
 
 __author__ = 'icoz'
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from functools import wraps
 from random import random
 
@@ -18,13 +18,14 @@ SECRET_KEY = 'sifdj ncs dcq odicn pdscn[os ncpasvaidcjn sajc acbqisbc csbabcdsac
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
 def login_required(func):
     @wraps(func)
     def decorated_view(*args, **kwargs):
         if session.get('logged_in'):
             return func(*args, **kwargs)
         else:
-            print('url=',request.url)
+            print('url=', request.url)
             session['next'] = request.url
             return redirect(url_for('login'), code=302)
 
@@ -109,7 +110,7 @@ def random_record():
     priority = int(random() * 7)
     apps = ['samba', 'exim', 'postfix', 'httpd', 'ftpd']
     app = apps[int(random() * len(apps))]
-    dt = str(datetime.today().timestamp())
+    dt = datetime.utcnow()
     words = ['hello', 'world', 'how', 'are', 'you']
     message = ' '.join([words[int(random() * len(words))] for i in range(5)])
     # h = host
@@ -127,6 +128,51 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/get_info', methods=['GET', 'POST'])
+@login_required
+def get_info():
+    hosts = get_hosts()
+    apps = get_apps()
+    data = None
+    app = None
+    host = None
+    act = dict()
+    if request.method == 'GET':
+        print('get-info: get')
+    else:
+        if request.args.get('skip'):
+            sk = request.args.get('skip')
+        else:
+            sk = 0
+        print('get-info: post', request.form)
+        host = request.form.get('host')
+        app = request.form.get('app')
+        prio = request.form.get('prio')
+        regex = request.form.get('msg_regex')
+        req = dict()
+        if host:
+            act['host'] = host
+            req['h'] = host
+        if app:
+            act['app'] = app
+            req['a'] = app
+        if prio is not None:
+            prio = int(prio)
+            act['prio'] = prio
+            req['p'] = prio
+        if regex:
+            act['regex'] = regex
+            req['m'] = {'$regex': regex}
+        print(req)
+        # if regex:
+        #     info = db.messages.find({'h': host, 'a': app, 'm':{'$regex': regex}}).limit(100+sk).skip(sk)
+        # else:
+        #     info = db.messages.find({'h': host, 'a': app}).limit(100+sk).skip(sk)
+        info = db.messages.find(req).limit(100 + sk).skip(sk)
+        data = [i for i in info]
+    return render_template('request_form.html', hosts=hosts, apps=apps, data=data, active=act)
+
+
 @app.route('/gen_info/<int:num>')
 @login_required
 def gen_info(num=1):
@@ -136,7 +182,8 @@ def gen_info(num=1):
         # out = d
         # print(d)
         h, f, p, a, d, m = random_record()
-        print(h, f, p, a, d, m)
+        d = d + timedelta(days=int(random() * 10) - 5)
+        # print(h, f, p, a, d, m)
         db['messages'].insert({'h': h, 'f': f, 'p': p, 'a': a, 'd': d, 'm': m})
         db['messages'].ensure_index('h')
         db['messages'].ensure_index('p')
@@ -157,36 +204,67 @@ def gen_info(num=1):
     return 'Done\n'
 
 
-@app.route('/servers/', methods=['GET', 'POST'])
-def servers():
-    print('servers')
+@app.route('/date/', methods=['GET', 'POST'])
+def test_date():
     if request.method == 'GET':
-        # collections = db.dates.find({}, {'coll_name': 1})
-        print('get')
-        info = db['dates'].aggregate({'$match': {'d': 0}})
+        info = db.messages.find({'d': {'$gte': datetime(2014, 5, 11, 10, 38, 10)}})
+        # info = db['dates'].aggregate({'$match': {'d': 0}})
         # info = db['dates'].aggregate({'$match': {'d': {'$and': [{'$gte':1}, {'$lte': 1}]}}},
         #                              {'$group': {"_id": 'servers', 'servers': {'$addToSet': '$servers'}}})
-        if info['ok']:
-            out = ""
-            servers = info['result'][0]['servers'][0]
-            print(servers)
-            for s in servers:
-                out += str(s) + "\n"
-        else:
-            print('not found')
-            out = 'not found'
-        return out
+        print(info.count())
+        ii = []
+        for i in info:
+            # print(i)
+            ii.append(str(i) + '<br>')
+        # if info['ok']:
+        #     out = ""
+        #     servers = info['result']
+        #     print(servers)
+        #     for s in servers:
+        #         out += str(s) + "\n"
+        # else:
+        #     print('not found')
+        #     out = 'not found'
+        return str(ii)
     else:
         date_from = request.json['date_from']
         date_to = request.json['date_to']
         collections = db.dates.find({'date': {'$ge': date_from, '$le': date_to}}, {'coll_name': 1})
-    for c in collections:
-        cn = c['coll_name']
-        print(cn)
-        map = ''
-        red = ''
-        db[cn].map_reduce(map=map, reduce=red)
-        db[cn].find({})
+        return 'post'
+    return 'error'
+
+
+def get_hosts():
+    return db.messages.distinct('h')
+
+
+def get_apps():
+    return db.messages.distinct('a')
+
+
+@app.route('/json/servers/', methods=['GET', 'POST'])
+def json_servers():
+    if request.method == 'GET':
+        return str(get_hosts())
+        # info = db.messages.aggregate({'$distinct': {'d': 0}})
+        # info = db['dates'].aggregate({'$match': {'d': {'$and': [{'$gte':1}, {'$lte': 1}]}}},
+        #                              {'$group': {"_id": 'servers', 'servers': {'$addToSet': '$servers'}}})
+    else:
+        return (str([]))
+        # date_from = request.json['date_from']
+        # date_to = request.json['date_to']
+        # collections = db.dates.find({'date': {'$ge': date_from, '$le': date_to}}, {'coll_name': 1})
+
+
+@app.route('/json/apps/', methods=['GET', 'POST'])
+def json_apps():
+    if request.method == 'GET':
+        return str(get_apps())
+        # info = db.messages.aggregate({'$distinct': {'d': 0}})
+        # info = db['dates'].aggregate({'$match': {'d': {'$and': [{'$gte':1}, {'$lte': 1}]}}},
+        #                              {'$group': {"_id": 'servers', 'servers': {'$addToSet': '$servers'}}})
+    else:
+        return (str([]))
 
 
 if __name__ == '__main__':
