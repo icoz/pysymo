9c99240310950bebf9f5f8b16cbfb0611dff7a9c
icoz <icoz.vt@gmail.com>
2014-12-27 01:32:45 +0300

    added PYSYMO_PRODUCTION for using in docker

--- run.py
+++ run.py
@@ -4,4 +4,9 @@
 __author__ = 'ilya-il'
 
 from app import app
-app.run(debug = True)
+from os import environ as env
+if env.get('PYSYMO_PRODUCTION'):
+  app.run(host='0.0.0.0', port=80)
+else:
+  app.run(debug = True, host='0.0.0.0', port=5000)
+
