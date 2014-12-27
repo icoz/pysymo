65934a6190a3bfe6d86f37ac1e02864bd0873680
icoz <icoz.vt@gmail.com>
2014-12-27 20:50:56 +0300

    fixes in tools/config.py and tool/init_medb.py for using ENV (for docker)

--- tools/config.py
+++ tools/config.py
@@ -6,12 +6,20 @@
 
 __author__ = 'ilya-il'
 
-MONGO_HOST = '127.0.0.1'
-MONGO_PORT = 27017
-MONGO_DATABASE = 'syslog'
+from os import environ as env
+MONGO_HOST = env.get('PYSYMO_MONGO_HOST') or env.get('DB_PORT_27017_TCP_ADDR') or '127.0.0.1'
+MONGO_PORT = int(env.get('PYSYMO_MONGO_PORT') or env.get('DB_PORT_27017_TCP_PORT')) or 27017
+MONGO_DATABASE = env.get('PYSYMO_MONGO_DATABASE') or 'syslog'
 
-PIPER_ERROR_LOG = '/var/log/pysymo/piper_error.log'
+# log file
+if sys.platform == 'win32':
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    PIPER_ERROR_LOG = os.path.join(basedir, 'piper_error.log')
+else:
+    PIPER_ERROR_LOG = os.environ.get('PIPER_ERROR_LOG') or '/var/log/pysymo/piper_error.log'
+
 
 # priority list
 # WARNING! do not change item position in list
 MSG_PRIORITY_LIST = ('emerg', 'alert', 'crit', 'err', 'warn', 'notice', 'info', 'debug')
+
